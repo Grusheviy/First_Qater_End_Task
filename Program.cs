e231b3c319e335b4f669d8ecab25ee8203354ba3
Ivan Tereshchenko <geminimzk@gmail.com>
2023-02-13 21:05:48 +0400

    Start making program and fill readme

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+string[] arrayOfStrings = new string[]  {};
+int lengthMax = 3;
+Console.WriteLine("Write your array length number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+arrayOfStrings = new string[n];
+for (int i = 0; i < arrayOfStrings.Length; i++)
+  {
+    Console.Write($" Write {i+1} string: ");
+    arrayOfStrings[i] = Console.ReadLine();
+  }
