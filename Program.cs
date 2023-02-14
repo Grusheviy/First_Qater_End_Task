4dd149c73d50f1cff8f92272b079236e73e1cbe4
Ivan Tereshchenko <geminimzk@gmail.com>
2023-02-14 10:50:10 +0400

    added method ArrayCheck for checking and counting how many array elements < 3

--- Program.cs
+++ Program.cs
@@ -2,13 +2,27 @@
 
 
 
-string[] arrayOfStrings = new string[]  {};
-int lengthMax = 3;
+
+
+
+
+int ArrayCheck(string[] array, int lengthMax)
+{
+  int result = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= lengthMax) result++;
+  }
+  return result;
+}
 Console.WriteLine("Write your array length number: ");
 int n = Convert.ToInt32(Console.ReadLine());
-arrayOfStrings = new string[n];
-for (int i = 0; i < arrayOfStrings.Length; i++)
+String[] ArrayOfStrings = new string[n];
+int lengthMax = 3;
+for (int i = 0; i < ArrayOfStrings.Length; i++)
   {
     Console.Write($" Write {i+1} string: ");
-    arrayOfStrings[i] = Console.ReadLine();
+    ArrayOfStrings[i] = Console.ReadLine();
   }
+int NumbesOfStrings = ArrayCheck(ArrayOfStrings, lengthMax);
+// Console.WriteLine($"{NumbesOfStrings}");
