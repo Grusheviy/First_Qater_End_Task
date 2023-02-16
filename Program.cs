320083ca6ed561cab18a601bed92d510110479da
Ivan Tereshchenko <geminimzk@gmail.com>
2023-02-16 10:47:25 +0400

    Ended final task

--- Program.cs
+++ Program.cs
@@ -15,12 +15,40 @@ int ArrayCheck(string[] array, int lengthMax)
   }
   return result;
 }
-
+void FillArray(string[] FirstArray, string[] ResultArray, int lengthMax)
+{
+  int temp = 0;
+  for (int i = 0; i < FirstArray.Length; i++)
+  {
+    if (FirstArray[i].Length <= lengthMax)
+    {
+      ResultArray[temp] = FirstArray[i];
+      temp++;
+    }
+  }
+}
+void PrintArray(string[] array)
+{ 
+  Console.Write("[");
+  for (int i = 0; i < array.Length; i++)
+    {
+      Console.Write('"' + array[i] +'"');
+        if (i < array.Length - 1)
+        {
+          Console.Write(",");
+        }
+    }
+  Console.Write("]");
+}
 
 Console.WriteLine("Write your array length number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 String[] ArrayOfStrings = new string[n];
 int lengthMax = 3;
+
+// Console.WriteLine("Write your result array symbol length: ");
+// int lengthMax = Convert.ToInt32(Console.ReadLine());
+
 for (int i = 0; i < ArrayOfStrings.Length; i++)
   {
     Console.Write($" Write {i+1} string: ");
@@ -28,3 +56,6 @@ for (int i = 0; i < ArrayOfStrings.Length; i++)
   }
 int NumbesOfStrings = ArrayCheck(ArrayOfStrings, lengthMax);
 // Console.WriteLine($"{NumbesOfStrings}");
+string[] ResultArrayOfStrings = new string[NumbesOfStrings];
+FillArray(ArrayOfStrings, ResultArrayOfStrings, lengthMax);
+PrintArray(ResultArrayOfStrings);
